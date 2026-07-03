58d72c6eb34d2345a7355b335679300a9e41de91
Aishwary <aishwary0212@gmail.com>
2026-07-04 03:33:34 +0530

    correct name

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
-const api = axios.create({ baseURL: VITE_API_URL ||"http://localhost:3000/api" });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+});
 
 const getStoredToken = () => {
   const rawAuth = localStorage.getItem("auth-storage");
